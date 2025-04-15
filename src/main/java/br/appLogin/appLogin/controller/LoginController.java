a98ef6bf29cba4db2381dd60912657d02d68ed98
Thiago Martins <158003020+ThiagoMartins2001@users.noreply.github.com>
2025-04-15 16:53:42 -0300

    sistema de coolie inclementado

--- src/main/java/br/appLogin/appLogin/controller/LoginController.java
+++ src/main/java/br/appLogin/appLogin/controller/LoginController.java
@@ -1,5 +1,7 @@
 package br.appLogin.appLogin.controller;
 
+import java.io.UnsupportedEncodingException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import br.appLogin.appLogin.model.User;
 import br.appLogin.appLogin.repository.UserRepository;
+import br.appLogin.appLogin.service.CookieService;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
@@ -36,17 +39,17 @@ public class LoginController {
     }
 
     @PostMapping("/logar")
-    public String loginuser(User usuario, Model model, HttpServletResponse response){
+    public String loginuser(User usuario, Model model, HttpServletResponse response) throws UnsupportedEncodingException{
         User userLogado = this.ur.login(usuario.getEmail(), usuario.getPassword());
         if(userLogado != null){
+            CookieService.setCookie (response, "usuarioLogado", String.valueOf(userLogado.getId()), 10000 );
+            CookieService.setCookie (response, "nomeusuario", String.valueOf (userLogado.getName()), 10000 );
           return "redirect:/";
         }
 
         model.addAttribute("erro", "Usuárrio ou senha inválidos!");
         return "login";
     }
-
-    //--------------------------------
 
     @GetMapping("/cadastrarUsuario")
     public String cadastro(){
---
+++ src/main/java/br/appLogin/appLogin/service/authenticator/LoginInterceptorAppConfig.java
@@ -0,0 +1,5 @@
+package br.appLogin.appLogin.service.authenticator;
+
+public class LoginInterceptorAppConfig {
+
+}
